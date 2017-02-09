a19fba2dda5fd74377595ee8f995b49064c4d055
Riva Syafri Rachmatullah <rivasyafri@gmail.com>
2017-02-09 20:06:22 +0700

    add google elevation template

---
+++ src/main/java/com/mofp/model/data/google/elevation/Location.java
@@ -0,0 +1,18 @@
+package com.mofp.model.data.google.elevation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author rivasyafri
+ */
+public class Location {
+
+    @Getter
+    @Setter
+    private double lat;
+
+    @Getter
+    @Setter
+    private double lng;
+}
